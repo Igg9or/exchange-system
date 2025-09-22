220ceaef4c35b0993da3ac6d00d2459a82fa2a81
Шпщк <yatmaskin02@mail.ru>
2025-09-22 18:45:14 +0300

    Render deploy setup

--- db.py
+++ db.py
@@ -1,12 +1,11 @@
-# db.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from contextlib import contextmanager
 
-# всегда подключаемся к локальной базе
-DATABASE_URL = "postgresql+psycopg2://exchange_user:1501@localhost:5432/exchange_db"
+url = os.environ.get("DATABASE_URL", "postgresql+psycopg2://postgres:1501@localhost/exchange_db")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(url, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
